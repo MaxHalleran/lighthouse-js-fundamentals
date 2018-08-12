43a134241d65f790284b20f78f9fa011f9f29c18
MaxHalleran <MaxHalleran@gmail.com>
2018-08-12 07:09:51 +0000

    LoopyLighthouse functions! it lives!

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,6 +1,15 @@
 function loopyLighthouse(start, end, num1, num2) {
   for (var i = start;i <= end; i++){
-    console.log(i);
+    if (i % num1 == 0 && i % num2 == 0) {
+      //could be i % (num1 * num2) == 0
+      console.log("LoopyLighthouse");
+    } else if (i % num1 == 0) {
+      console.log("Loopy");
+    } else if (i % num2 == 0) {
+      console.log("Lighthouse");
+    } else {
+      console.log(i);
+    }
   }
 }
 
